2fc12a6fce20521e6ad80ce6d08c6844c7c77b0c
mohanjnv1 <mohanjnv1@gmail.com>
2019-10-19 14:47:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notify/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.notify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Switch;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String CHANNEL_ID = "mohan";
+    private static final String CHANNEL_NAME = "mohan";
+    private static final String CHANNEL_DESC = "mohan notification";
+    private Button btn;
+
+    private EditText email;
+    private EditText password;
+    private Button login;
+    //    private Button register;
+    private ProgressBar progressBar;
+    private Switch mess,sport,gen;
+//    private FirebaseAuth mAuth=FirebaseAuth.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mess= findViewById(R.id.mess);
+        sport = findViewById(R.id.sports);
+        gen = findViewById(R.id.general);
+        btn = findViewById(R.id.button);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mess.isChecked()){
+                    FirebaseMessaging.getInstance().subscribeToTopic("mess");
+                }else{
+                    FirebaseMessaging.getInstance().unsubscribeFromTopic("mess");
+                }
+                if(sport.isChecked()){
+                    FirebaseMessaging.getInstance().subscribeToTopic("sports");
+                }else{
+                    FirebaseMessaging.getInstance().unsubscribeFromTopic("sports");
+                }
+                if(gen.isChecked()){
+                    FirebaseMessaging.getInstance().subscribeToTopic("general");
+                }else{
+                    FirebaseMessaging.getInstance().unsubscribeFromTopic("general");
+                }
+
+            }
+        });
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription(CHANNEL_DESC);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+    }
+}
